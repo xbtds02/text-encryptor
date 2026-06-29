a0c405233b5872526c0c9a157596976cabae7088
小柏兔 <1484171726@qq.com>
2026-06-30 02:32:26 +0800

    Add/update main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webSecurity: true,
+    },
+    icon: path.join(__dirname, 'icon.png'),
+    title: '文字隐写工具',
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  // 开发环境可打开开发者工具
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
