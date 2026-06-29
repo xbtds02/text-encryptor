b07bc508ce889cbdac4315dbc6299433de98b9df
小柏兔 <1484171726@qq.com>
2026-06-30 02:18:37 +0800

    Add PWA support and GitHub Actions workflow to build Android APK

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'text-encryptor-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
